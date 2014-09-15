6bee411cca2d5cb66157ee161253f9c1a63e77c7
Nhat Tran <thmnhat@gmail.com>
2014-09-15 12:55:38 +0700

    Fix wrong structure

--- varnish-vmod-cookie.rb
+++ varnish-vmod-cookie.rb
@@ -1,4 +1,5 @@
-require "formula"
+require 'formula'
+require 'varnish'
 
 class VarnishVmodCookie < Formula
   init
@@ -7,13 +8,17 @@ class VarnishVmodCookie < Formula
   sha1 '66230f4c0c2128c3fb6f57a8cb41f8628361c280'
   head 'https://github.com/lkarsten/libvmod-cookie.git'
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
+  depends_on 'libtool' => :build
   depends_on 'pkg-config' => :build
   depends_on 'pcre'
   depends_on 'varnish'
 
   def install
+    system "./autogen.sh"
+    system "./configure"
+    system "./make"
+    system "./make", "install"
   end
 end
